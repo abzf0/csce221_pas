e9266653d9c6c5fba749affb6608bd82900b15dc
flu0r1ne <flu0r1ne@flu0r1ne.net>
2022-01-27 01:02:28 -0600

    Add swap method, fix makefile

--- src/sorting.h
+++ src/sorting.h
@@ -1,41 +1,43 @@
 #pragma once
 
 #include <functional> // std::less
+#include <iterator>
 
 // This is C++ magic which will allows our function
-// to default to using a < b if a comparator is not
-// specified. Essentially, it defines std::less<T>
+// to default to using a < b if the comparator arg
+// is unspecified. It uses defines std::less<T>
 // for the iterator's value_type.
 //
-// If you have a vector<float>, the iterator's value
-// type will be float. std::less will select the <
-// for sorting floats as the default comparator.
+// For example: if you have a vector<float>, the 
+// iterator's value type will be float. std::less 
+// will select the < for sorting floats as the 
+// default comparator.
 
 namespace {
 	template<typename RandomIter>
-	using less_for_iter = std::less<typename RandomIter::value_type>; 
+	using less_for_iter = std::less<typename std::iterator_traits<RandomIter>::value_type>;
+
+	/* Efficiently swap two items - use this to implement your sorts */
+	template<typename T>
+	void swap(T & a, T & b) noexcept { /* COMPLETE */ }
 }
 
 template<typename RandomIter, typename Comparator = less_for_iter<RandomIter>>
 void bubble_sort(RandomIter begin, RandomIter end, Comparator comp = Comparator{}) {
-	// You can add more of these aliases if necessary
 	// Random access iterators have the same traits you defined in the Vector class
-	// difference_type represents a pointer difference
-	using difference_type = typename RandomIter::difference_type;
+	// For instance, difference_type represents an iterator difference
+	// You may delete the types you don't use to remove the compiler warnings
+	using _it             = std::iterator_traits<RandomIter>;
+	using difference_type = typename _it::difference_type;
+	using value_type      = typename _it::value_type;
+	using reference       = typename _it::reference;
+	using pointer         = typename _it::pointer;
 
 	// COMPLETE
 }
 
 template<typename RandomIter, typename Comparator = less_for_iter<RandomIter>>
-void insertion_sort(RandomIter begin, RandomIter end, Comparator comp = Comparator{}) {
-	using difference_type = typename RandomIter::difference_type;
-
-    // COMPLETE
-}
+void insertion_sort(RandomIter begin, RandomIter end, Comparator comp = Comparator{}) { /* COMPLETE */ }
 
 template<typename RandomIter, typename Comparator = less_for_iter<RandomIter>>
-void selection_sort(RandomIter begin, RandomIter end, Comparator comp = Comparator{}) {
-	using difference_type = typename RandomIter::difference_type;
-
-    // COMPLETE
-}
+void selection_sort(RandomIter begin, RandomIter end, Comparator comp = Comparator{}) { /* COMPLETE */ }
