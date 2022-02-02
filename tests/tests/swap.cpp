c4c234669b460ea598817ed56c3f5f79d89d3101
pulchroxloom <code@dantebarbieri.dev>
2022-02-01 20:44:27 -0600

    Rename variables

--- tests/tests/swap.cpp
+++ tests/tests/swap.cpp
@@ -4,17 +4,17 @@
 TEST(swap) {
     Typegen t;
     for(size_t sz = 0; sz < 100; ++sz) {
-        std::vector<int> a_original(sz);
         std::vector<int> a_gt(sz);
-        std::vector<int> b_original(sz);
+        std::vector<int> a(sz);
         std::vector<int> b_gt(sz);
+        std::vector<int> b(sz);
         for(size_t i = 0; i < sz; ++i) {
-            a_original[i] = a_gt[i] = t.get<int>();
-            b_original[i] = b_gt[i] = t.get<int>();
+            a_gt[i] = a[i] = t.get<int>();
+            b_gt[i] = b[i] = t.get<int>();
         }
         {
             Memhook mh;
-            sort::swap(a_original, b_original);
+            sort::swap(a_gt, b_gt);
             if (mh.n_allocs() != 0) {
                 std::cerr << "ERROR: Swap is allocating a new value.\n"
                     << "\tDoes your swap use copy or move semantics?\n";
@@ -25,17 +25,17 @@ TEST(swap) {
                     << "\tShould swap do this?\n";
             }
             ASSERT_EQ(0UL, mh.n_frees());
-            if (a_original != b_gt) {
-                std::cerr << "ERROR: b_gt does not contain the same values that a_original contains.\n"
+            if (a_gt != b) {
+                std::cerr << "ERROR: b does not contain the same values that a_gt contains.\n"
                     << "\tDid you swap the values?\n";
             }
-            if (b_original != a_gt) {
-                std::cerr << "ERROR: a_gt does not contain the same values that b_original contains.\n"
+            if (b_gt != a) {
+                std::cerr << "ERROR: a does not contain the same values that b_gt contains.\n"
                     << "\tDid you swap the values?\n";
             }
             for(size_t i = 0; i < sz; ++i) {
-                ASSERT_EQ(a_original[i], b_gt[i]);
-                ASSERT_EQ(b_original[i], a_gt[i]);
+                ASSERT_EQ(a_gt[i], b[i]);
+                ASSERT_EQ(b_gt[i], a[i]);
             }
         }
     }
