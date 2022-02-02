672e0bb036fca6f2d330a8d8adf088cf837cf428
Carlos Alvarez del Castillo <carlosadelcs@tamu.edu>
2022-02-01 19:53:42 -0600

    Checks values of swapped data to make sure it matches the originals

--- tests/tests/swap.cpp
+++ tests/tests/swap.cpp
@@ -4,15 +4,21 @@
 TEST(swap) {
     Typegen t;
     for(size_t sz = 0; sz < 100; ++sz) {
-        std::vector<int> a(sz);
-        std::vector<int> b(sz);
+        std::vector<int> a_original(sz);
+        std::vector<int> a_gt(sz);
+        std::vector<int> b_original(sz);
+        std::vector<int> b_gt(sz);
         for(size_t i = 0; i < sz; ++i) {
-            a[i] = t.get<int>();
-            b[i] = t.get<int>();
+            a_original[i] = a_gt[i] = t.get<int>();
+            b_original[i] = b_gt[i] = t.get<int>();
         }
         Memhook mh;
-        swap(a, b);
+        swap(a_original, b_original);
         ASSERT_EQ(0UL, mh.n_allocs());
         ASSERT_EQ(0UL, mh.n_frees());
+        for(size_t i = 0; i < sz; ++i) {
+            ASSERT_EQ(a_original[i], b_gt[i]);
+            ASSERT_EQ(b_original[i], a_gt[i]);
+        }
     }
 }
